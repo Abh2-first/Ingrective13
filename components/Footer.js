9c1524516edb24c1a1f7535ec7edf81527bbbbd0
Abh2-first <abhaykaliyar20425@gmail.com>
2025-10-07 15:38:42 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white shadow-inner py-4 mt-10 text-center text-sm text-gray-600">
+      © {new Date().getFullYear()} Team Ingrective — “Can Read and Eat”
+    </footer>
+  );
+}
